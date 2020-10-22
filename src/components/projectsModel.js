802a72f8fa11b9d0756405e2935b2880b3961a74
Susan Chesnut <scc0019@tigermail.auburn.edu>
2020-10-22 12:56:08 -0500

    updated packages and text

--- src/components/projectsModel.js
+++ src/components/projectsModel.js
@@ -31,7 +31,7 @@ export const projectsModelConfig = {
         },{
             "blurb": "After releasing website!"
         }],
-        "description": "I have known that I would like a new job for some time, but I always seemed to get bogged down in the details. A couple months ago I was sitting in an agile workshop and the idea came to me - why can't I apply this to finding a new job? I knew what I wanted to accomplish but needed help with the structure. This framework provided a written down plan and the ability to watch my progress - two things that are big motivators for me. All of this with the added benefit of practicing agile principles too! I initially had a handful of job related tasks/PBIs. As I knocked those off, I saw myself get more motivated and begin to scope out more items. Early on, I realized that Scrum wouldn't be the ideal approach so I adopted Kanban instead. I'm continuing to refine my process, but I've already accomplished more since I started than I had in the prior 2 years combined."
+        "description": "I had known that I would like a new job for some time, but I always seemed to get bogged down in the details. Then I was sitting in an agile workshop and the idea came to me - why can't I apply this to finding a new job? I knew what I wanted to accomplish but needed help with the structure. This framework provided a written down plan and the ability to watch my progress - two things that are big motivators for me. All of this with the added benefit of practicing agile principles too! I initially had a handful of job related tasks/PBIs. As I knocked those off, I saw myself get more motivated and begin to scope out more items. Over the last year, this process has helped me achieve a lot of my goals."
     },
     "parachute": {
         "name": "parachute", 
@@ -43,18 +43,6 @@ export const projectsModelConfig = {
         },{
             "blurb": "My Favorite Skills"
         }],        
-        "description": "After admitting that I wasn't sure I was in the right field to an instructor a few years ago, he recommended I read <i>What Color is Your Parachute</i> by Richard Nelson Bolles. I looked at the book later that night, even bought an e-book version, but it required a fairly extensive self-inventory to help the reader figure out what they wanted and 'I just didn't have the time'. Fast forward to early this year - I was ready to bite the bullet and find a new job. And not just any job - I wanted a job I'd love and enjoy, something I'm passionate about. So, I re-read the book, created my own workbook for the self-inventory and spent a few months going through it. And I'm glad I did. It has further inspired me to look for a new job and to not settle for one that falls short of my key passions and skills. The images on the left are a few pages from the completed workbook."
-    },
-    "plantMonitoring": {
-        "name": "plantMonitoring", 
-        "header": "Plant Monitoring Web Application",
-        "imageInformation": [{
-            "blurb": "Prime candidate for soil & fruit monitoring"
-        },{
-            "blurb": "A stressed cactus - a use case for a user alert"
-        },{
-            "blurb": "Goal: identify plants 'in the wild'"
-        }],
-        "description": "As you can probably tell from the rest of my site, I have a thing for plants. I continue to expand my balcony garden and check it every day; I 'blame' my parents and growing up on a farm for this love of growing things. <br/><br/>As I was trying to remember if I needed to fertilize or just water one day, I recalled learning about a Microsoft project that recognizes plants and recommends certain measures for more efficient growth/production. I started talking to my friend, and we came up with this idea to create our own app that would monitor a garden, recognize the plants and best care for them, and notify the user as needed (fertilize, weather, plant needs care, etc). Around this time, I finished up the parachute book and realized that something like this at large scale - pairing technology with my love of plants - would be my ideal career. My friend and I are still ironing out details, but I am really looking forward to working on this. There's a lot to learn!"
+        "description": "After admitting that I wasn't sure I was in the right field to an instructor a few years ago, he recommended I read <i>What Color is Your Parachute</i> by Richard Nelson Bolles. I looked at the book later that night, even bought an e-book version, but it required a fairly extensive self-inventory to help the reader figure out what they wanted and 'I just didn't have the time'. Fast forward a year - I was ready to bite the bullet and find a new job. And not just any job - I wanted a job I'd love and enjoy, something I'm passionate about. So, I re-read the book, created my own workbook for the self-inventory and spent a few months going through it. And I'm glad I did. It further inspired me to look for a new job and to not settle for one that falls short of my key passions and skills. The images on the left are a few pages from the completed workbook."
     }
 }
